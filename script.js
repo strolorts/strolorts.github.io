0f6d6b3b2a8750d543628e1e9bffd5313cc649a8
strolorts <strolorts@gmail.com>
2021-05-26 20:50:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function Button1(el) {
 	//el.style.color = "yellow"
 	var buttonk = Math.floor(Math.random( ) * (3 - 1 + 1)) + 1
 	if (buttonk == 1) {
-		document.write('<img src="1.png">');
+		document.write('<img src="1.PNG">');
 	}
 	else if (buttonk == 2) {
-		document.write('<img src="2.png">');
+		document.write('<img src="2.PNG">');
 	}
 	else if (buttonk == 3) {
-		document.write('<img src="3.png">');
+		document.write('<img src="3.PNG">');
 	}
 }
