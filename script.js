d725e5bc5a73bb3231db74b731406f49ef054577
strolorts <54234103+strolorts@users.noreply.github.com>
2021-05-27 17:14:04 +0300

    лiчба

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function Button1(el) {
-	//el.style.background = "black";
-	//el.style.color = "yellow"
-	var buttonk = Math.floor(Math.random( ) * (3 - 1 + 1)) + 1
-	if (buttonk == 1) {
+	var buttonk = Math.floor(Math.random( ) * 3) 
+	if (buttonk == 0) {
 		document.write('<img src="https://raw.githubusercontent.com/strolorts/strolorts.github.io/main/1.PNG">');
 	}
-	else if (buttonk == 2) {
+	else if (buttonk == 1) {
 		document.write('<img src="https://raw.githubusercontent.com/strolorts/strolorts.github.io/main/2.PNG">');
 	}
-	else if (buttonk == 3) {
+	else if (buttonk == 2) {
 		document.write('<img src="https://raw.githubusercontent.com/strolorts/strolorts.github.io/main/3.PNG">');
 	}
-	var audio = new Audio(); // Создаём новый элемент Audio
+	var audio = new Audio();
 	audio.src = 'https://my-files.su/Save/8inabz/SUS%20-%20AMOGUS.mp3'; // Указываем путь к звуку "клика"
 	audio.autoplay = true; // Автоматически запускаем		
 }
